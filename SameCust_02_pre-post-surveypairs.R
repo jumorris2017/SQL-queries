f3582bc93f5821f8b6b600f44f3d92fb49ff036b
jumorris2017 <33705848+jumorris2017@users.noreply.github.com>
2017-12-14 14:34:51 -0800

    12.14.17_samecustR

--- SameCust_02_pre-post-surveypairs.R
+++ SameCust_02_pre-post-surveypairs.R
@@ -10,4 +10,80 @@ library(xlsx)
 library(ggplot2)
 
 #load data
-scus <- fread("O:/CoOp/CoOp194_PROReportng&OM/Julie/SameCust_02_pre-post-surveypairs.csv")
+scus <- read.csv("O:/CoOp/CoOp194_PROReportng&OM/Julie/SameCust_02_pre-post-surveypairs.csv")
+setDT(scus)
+
+##remove once no longer person-date-level
+scus[, c("GUID_USER_ID","CASE_ID_1","TRANS_DTM_1","TRANS_ID_1",
+         "CASE_ID_2","TRANS_DTM_2","TRANS_ID_2") := NULL]
+
+
+#ANALYSIS 1: FOCUS ONLY ON POST-PERIODS
+
+#create subsetting variable for change in CC score
+scus[CC_1==7&CC_2==7, ccdelgrp := "7to7"]
+scus[CC_1==7&CC_2==6, ccdelgrp := "7to6"]
+scus[CC_1==7&CC_2<=5, ccdelgrp := "7to5orless"]
+scus[CC_1==6&CC_2==6, ccdelgrp := "6to6"]
+scus[CC_1==6&CC_2==5, ccdelgrp := "6to5"]
+scus[CC_1==6&CC_2<=4, ccdelgrp := "6to4orless"]
+scus[CC_1<=5&CC_2<=5, ccdelgrp := "5orlessto5orless"]
+scus[CC_2==7&CC_2==6, ccdelgrp := "6to7"]
+scus[CC_2==7&CC_2<=5, ccdelgrp := "5orlessto7"]
+scus[CC_2==6&CC_2==5, ccdelgrp := "5to6"]
+scus[CC_2==6&CC_2<=4, ccdelgrp := "4orlessto6"]
+
+#assess differences in behavior across *groups* of people based on ccdelgrp
+temp <- scus[, c(VISITS_POST_15D_1 = sum(VISITS_POST_15D_1),
+                 VISITS_POST_15D_2 = sum(VISITS_POST_15D_2),
+                 SPEND_POST_15D_1 = sum(SPEND_POST_15D_1),
+                 SPEND_POST_15D_2 = sum(SPEND_POST_15D_2)),
+             by=c("ccdelgrp")]
+temp[, visdelta := VISITS_POST_15D_2 - VISITS_POST_15D_1]
+temp[, spenddelta := SPEND_POST_15D_2 - SPEND_POST_15D_1]
+
+#assess differences in behavior by time 1 CC and time 2 CC
+temp2 <- scus[, c(VISITS_POST_15D_1 = sum(VISITS_POST_15D_1),
+                 VISITS_POST_15D_2 = sum(VISITS_POST_15D_2),
+                 SPEND_POST_15D_1 = sum(SPEND_POST_15D_1),
+                 SPEND_POST_15D_2 = sum(SPEND_POST_15D_2)),
+             by=c("CC_1","CC_2")]
+temp2[, visdelta := VISITS_POST_15D_2 - VISITS_POST_15D_1]
+temp2[, spenddelta := SPEND_POST_15D_2 - SPEND_POST_15D_1]
+
+#ANALYSIS 2: LOOK AT VISIT AND SPEND PRE-TO-POST DELTAS
+
+#assess differences in behavior across *groups* of people based on ccdelgrp
+temp <- scus[, c(VISITS_PRE_15D_1 = sum(VISITS_PRE_15D_1),
+                 VISITS_PRE_15D_2 = sum(VISITS_PRE_15D_2),
+                 VISITS_POST_15D_1 = sum(VISITS_POST_15D_1),
+                 VISITS_POST_15D_2 = sum(VISITS_POST_15D_2),
+                 SPEND_PRE_15D_1 = sum(SPEND_PRE_15D_1),
+                 SPEND_PRE_15D_2 = sum(SPEND_PRE_15D_2),
+                 SPEND_POST_15D_1 = sum(SPEND_POST_15D_1),
+                 SPEND_POST_15D_2 = sum(SPEND_POST_15D_2)),
+             by=c("ccdelgrp")]
+temp[, visdelta_1 := VISITS_POST_15D_1 - VISITS_PRE_15D_1]
+temp[, visdelta_2 := VISITS_POST_15D_2 - VISITS_PRE_15D_2]
+temp[, spenddelta_1 := SPEND_POST_15D_1 - SPEND_PRE_15D_1]
+temp[, spenddelta_2 := SPEND_POST_15D_2 - SPEND_PRE_15D_2]
+
+#assess differences in behavior by time 1 CC and time 2 CC
+temp2 <- scus[, c(VISITS_PRE_15D_1 = sum(VISITS_PRE_15D_1),
+                 VISITS_PRE_15D_2 = sum(VISITS_PRE_15D_2),
+                 VISITS_POST_15D_1 = sum(VISITS_POST_15D_1),
+                 VISITS_POST_15D_2 = sum(VISITS_POST_15D_2),
+                 SPEND_PRE_15D_1 = sum(SPEND_PRE_15D_1),
+                 SPEND_PRE_15D_2 = sum(SPEND_PRE_15D_2),
+                 SPEND_POST_15D_1 = sum(SPEND_POST_15D_1),
+                 SPEND_POST_15D_2 = sum(SPEND_POST_15D_2)),
+             by=c("CC_1","CC_2")]
+temp2[, visdelta_1 := VISITS_POST_15D_1 - VISITS_PRE_15D_1]
+temp2[, visdelta_2 := VISITS_POST_15D_2 - VISITS_PRE_15D_2]
+temp2[, spenddelta_1 := SPEND_POST_15D_1 - SPEND_PRE_15D_1]
+temp2[, spenddelta_2 := SPEND_POST_15D_2 - SPEND_PRE_15D_2]
+
+temp2[, visdelta := VISITS_POST_15D_2 - VISITS_POST_15D_1]
+temp2[, spenddelta := SPEND_POST_15D_2 - SPEND_POST_15D_1]
+
+#
