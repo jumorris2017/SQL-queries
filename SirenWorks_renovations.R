624baa41dd91156af6612f8deed18899a8f70b0e
jumorris2017 <33705848+jumorris2017@users.noreply.github.com>
2018-02-27 17:11:15 -0800

    2.27.18

--- SirenWorks_renovations.R
+++ SirenWorks_renovations.R
@@ -8,6 +8,8 @@ library(data.table)
 library(ggplot2)
 library(tidyverse)
 library(lubridate)
+library(PerformanceAnalytics)
+library(ggthemes)
 
 #group by stores that *are* or *are not* using the new plays
 
@@ -16,20 +18,76 @@ library(lubridate)
 swce <- fread("Q:/Departments/WMO/Marketing Research/New Q drive/Foundational/Customer Voice/2.0/Ad Hoc Q/2018_2_20_Siren Works Renovations/ce101_2017.csv")
 swce[, dateymd := ymd(date1)]
 swce[, datemonth := month(dateymd)]
-swce[adhoc==1, testcase := 1];swce[adhoc==2, testcase := 0]
+swce[, datetime := ymd_hms(sbid_date)]
+# swce[adhoc==1, testcase := 1];swce[adhoc==2, testcase := 0]
 
 #reduce number of variables
-swce <- swce[, c("guid","dateymd","datemonth","testcase","q1",grep("q2",colnames(swce),value=T)),with=FALSE]
+swce <- swce[, c("guid","dateymd","datemonth","stid","q1",grep("q2",colnames(swce),value=T)),with=FALSE]
+
+#initial analyses:
+#1-correlations between adhocs and other CE questions
+#2-N by store
+temp <- swce[, list(nguid = length(unique(guid))), by="stid"]
+average(temp[,nguid])
+hist(temp[,nguid])
+
+#set labels
+xlabel <- "Number of Surveys"
+ylabel <- "Number of Stores"
+tlabel <- "CE survey count per store"
+sublabel <- "SirenWorks ad hoc questions"
+caption <- "Store N = 13,997\nSurvey N = 715,591"
+#plot itself
+plot2 <- ggplot(temp,aes(nguid)) + 
+  geom_histogram(binwidth=1,show.legend=FALSE,fill="lightgrey",col=I("black")) + 
+  theme_economist() + scale_colour_brewer(palette = 1, name="", labels="") +
+  #scale_x_continuous(limits=c(-35,35), breaks = scales::pretty_breaks(35)) +
+  xlab(xlabel) + ylab(ylabel) + ggtitle(tlabel) + labs(subtitle=sublabel,caption=caption)
+print(plot2)
+
+###correlation matrices
+#set up functions
+swcecorrmat <- swce[, c("q1",grep("q2",colnames(swce),value=T)),with=FALSE]
+
+## correlation matrix with p-values
+cor.prob <- function (X, dfr = nrow(X) - 2) {
+  R <- cor(X, use="pairwise.complete.obs")
+  above <- row(R) < col(R)
+  r2 <- R[above]^2
+  Fstat <- r2 * dfr/(1 - r2)
+  R[above] <- 1 - pf(Fstat, 1, dfr)
+  R[row(R) == col(R)] <- NA
+  R
+}
+## create function to dump the cor.prob output to a 4 column matrix
+## with row/column indices, correlation, and p-value.
+flattenSquareMatrix <- function(m) {
+  if( (class(m) != "matrix") | (nrow(m) != ncol(m))) stop("Must be a square matrix.") 
+  if(!identical(rownames(m), colnames(m))) stop("Row and column names must be equal.")
+  ut <- upper.tri(m)
+  data.frame(i = rownames(m)[row(m)[ut]],
+             j = rownames(m)[col(m)[ut]],
+             cor=t(m)[ut],
+             p=m[ut])
+}
+#flatten the table
+cm <- flattenSquareMatrix(cor.prob(swcecorrmat))
+write.csv(cm,file="O:/CoOp/CoOp194_PROReportng&OM/Julie/CE_SirenWorks_corrmatrix.csv")
+
+
+
 
 #pull in cust trans data
 ctrans <- fread("O:/CoOp/CoOp194_PROReportng&OM/Julie/CE_SirenWorks_custtrans.csv")
 setnames(ctrans,c("GUID_ID"),c("guid"))
+ctrans[, datetime := ymd_hms(RSPNS_DT)]
 ctrans[, datemonth := FSCL_PER_IN_YR_NUM-3]
 
 #swing wide by month
-ctrans <- dcast.data.table(ctrans, guid ~ datemonth, value.var="TRANS")
-colnames(ctrans)[2:5] <- paste("transmnth", colnames(ctrans)[2:5], sep = "_")
+ctrans <- dcast.data.table(ctrans, guid + datetime + STORE_NUM ~ datemonth, value.var="TRANS")
+colnames(ctrans)[4:7] <- paste("transmnth", colnames(ctrans)[2:5], sep = "_")
 
 #merge
-temp <- merge(swce,ctrans,by="guid")
+temp <- merge(swce,ctrans,by=c("guid"))
+
 
--- Transaction_Count_by_Customer_adhoc_SirenWorks__SBP416.sql
+++ Transaction_Count_by_Customer_adhoc_SirenWorks__SBP416.sql
@@ -1,8 +1,12 @@
+                         
+
 /* Pulls customer transaction count by GUID for customers in the CE survey data */
 
 select b.FSCL_YR_NUM
   ,b.FSCL_PER_IN_YR_NUM
   ,a.GUID_ID
+  --,sq.RSPNS_DT
+  --,sq.STORE_NUM
   ,count(*) AS TRANS
   --,sq.TOTAL_TB
   --,sq.TOTAL_RSPNS
@@ -23,6 +27,8 @@ JOIN APPCA.D_STORE_VERS st
 
 inner join (SELECT
   sr.GUID_USER_ID
+  --,sr.RSPNS_DT
+  --,sr.STORE_NUM
   --,SUM(CASE WHEN sr.RSPNS_ID = '7' AND sr.QSTN_ID = 'Q2_2' THEN 1 ELSE 0 END) AS TOTAL_TB
   --,SUM(CASE WHEN sr.QSTN_ID = 'Q2_2' THEN 1 ELSE 0 END) AS TOTAL_RSPNS
   ,ca.FSCL_YR_NUM
@@ -38,6 +44,8 @@ WHERE ((TRUNC(sr.TRANS_DTM) BETWEEN '08-MAR-17' AND '22-MAR-17') OR (TRUNC(sr.TR
   AND sr.RSPNS_ID <> '9'
 GROUP BY 
   sr.GUID_USER_ID
+  --,sr.RSPNS_DT
+  --,sr.STORE_NUM
   ,ca.FSCL_YR_NUM
   ,ca.FSCL_PER_IN_YR_NUM
   ,TRUNC(sr.TRANS_DTM)
@@ -50,6 +58,8 @@ where c.SVC_INTRNL_RQST_NM = 'Redemption'
 group by b.FSCL_YR_NUM
   ,b.FSCL_PER_IN_YR_NUM
   ,a.GUID_ID
+  --,sq.RSPNS_DT
+  --,sq.STORE_NUM
   --,sq.TOTAL_TB
   --,sq.TOTAL_RSPNS
 ORDER BY 
