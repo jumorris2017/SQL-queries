643f48dcd44fd44e9b0ac19a8a5fe4335ec9db74
jumorris2017 <33705848+jumorris2017@users.noreply.github.com>
2018-03-15 17:15:05 -0700

    3.15.18

--- SCAP_ROI.sql
+++ SCAP_ROI.sql
@@ -13,3 +13,34 @@ select * from SCAP_LOG_NEW
     
     
     
+select 
+ 
+a.FP_End,
+a.[Job_Number],
+--a.[Personnel Number],
+--a.SIP_Elig,
+--a.SIP_Part,
+SUM(a.SIP_Elig) As [SE],
+SUM(a.SIP_Part) As [SP],
+SUM(Convert(decimal(12,2),a.SIP_Part))/SUM(Convert(decimal(12,2),a.SIP_Elig)) As [SIP Rate],
+
+SUM(a.BFT_Elig) As [BE],
+SUM(a.BFT_Part) As [BP],
+SUM(Convert(decimal(12,2),a.BFT_Part))/SUM(Convert(decimal(12,2),a.BFT_Elig)) As [BFT Rate],
+
+SUM(a.RET_Elig) As [RE],
+SUM(a.RET_Part) As [RP],
+SUM(Convert(decimal(12,2),a.RET_Part))/SUM(Convert(decimal(12,2),a.RET_Elig)) As [RET Rate]
+
+--a.RET_Elig,
+--a.RET_Part
+
+from hpohlman.PRO_BaseData a
+
+where a.FP_End = '02-25-2017'
+and a.Action_Group = 'headcount'
+and a.[Country] = 'US'
+and a.BFT_elig = 1
+and a.[Job_Number] In(50000362,50000358,50000117)
+
+Group By a.FP_End,a.[Job_Number]
