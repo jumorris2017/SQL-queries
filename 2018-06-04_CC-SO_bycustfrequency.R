78ca39b5663a49eebcb452686efe407a25499f46
Julie Morris <jumorris@starbucks.com>
2018-06-05 17:01:55 -0700

    6.5.18

--- 2018-06-04_CC-SO_bycustfrequency.R
+++ 2018-06-04_CC-SO_bycustfrequency.R
@@ -16,7 +16,9 @@ ce <- ce[FSCL_YR_NUM==2018&FSCL_PER_IN_YR_NUM==7]
 ce[TRANS<=5, vis_bin := 1]
 ce[TRANS>=6&TRANS<=10, vis_bin := 2]
 ce[TRANS>=11&TRANS<=15, vis_bin := 3]
-ce[TRANS>=16, vis_bin := 4]
+ce[TRANS>=16&TRANS<=20, vis_bin := 4]
+ce[TRANS>=21&TRANS<=25, vis_bin := 5]
+ce[TRANS>=26, vis_bin := 6]
 
 #aggregate by vis_bin and question
 ce <- ce[, list(USER_COUNT = sum(USER_COUNT,na.rm=T),
@@ -31,20 +33,20 @@ ce[QSTN_ID!='Q2_2', variable := 1];ce[QSTN_ID=='Q2_2', variable := 0]
 
 #average of averages for so score
 ceagg <- ce[, list(n = max(USER_COUNT,na.rm=T),
-                tbscore = round(mean(tbscore,na.rm=T),3)*100),
-         by=c("vis_bin","variable")]
+                   tbscore = round(mean(tbscore,na.rm=T),3)*100),
+            by=c("vis_bin","variable")]
 
 #plot 1
 #set labels
 # xlabel <- "Proven SR"
 xlabels <- c("Customer Connection","Store Operations")
 ylabel <- "Top Box Score"
-tlabel <- "Customer Experience by Customer Visitation Frequency"
+tlabel <- "Customer Experience by Visitation Frequency"
 sublabel <- "Customer Connection & Store Operations"
 caption <- "Customer Experience Survey and SR Visitation, April FY18"
 #manual legend labels
 lname <- "30-Day Visitation"
-llabels <- c("1-5", "6-10", "11-15", "16+")
+llabels <- c("1-5", "6-10", "11-15", "16-20", "21-25", "26+")
 #values
 pdata1a <- ceagg
 px1a <- ceagg[,variable]
@@ -56,7 +58,7 @@ plot1a <- ggplot(data=pdata1a,aes(y=py1a,x=as.factor(px1a),fill=as.factor(groupv
   geom_bar(stat="identity", width = 0.95, position=position_dodge(), colour="black") +
   scale_fill_brewer(palette = 1, name=lname, labels=llabels) + theme_economist_white(gray_bg = FALSE) +
   scale_x_discrete(name="",labels=xlabels) +
+  scale_y_continuous(limits=c(0,100)) +
   xlab("") + ylab(ylabel) + ggtitle(tlabel) + labs(subtitle=sublabel,caption=caption) +
   geom_text(size = 3.5, aes(label=py1a,y=0), stat="identity", vjust = -1, position = position_dodge(0.95)) 
-  #geom_text(size = 2.5, aes(label=paste0("n=",nvar1a),y=0), stat= "identity", vjust = -1, position = position_dodge(0.95)) 
 print(plot1a)
